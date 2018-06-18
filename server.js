65a83d70cdd640dedc3d3bccd15b7c3551079165
Joseph Cowsert <cowsertjoe@gmail.com>
2018-06-17 19:11:05 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) =>{
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) =>{
+        if(err){
+            console.log('Unable to append to server.log.');
+        }
+    }); next();
+});
+
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs',{
+//         pageTitle: "We'll be right back",
+//         welcomeMessage: 'Site is under constructions. Please come back later'
+//     });
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle:'Home Page',
+        welcomeMessage: 'Welcome to our website!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+// /bad - simulates what a request fail
+//send back json with error message property
+app.get('/bad', (req, res) => {
+    res.send({errorMessage: 'Unable to be awesome'});
+
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
